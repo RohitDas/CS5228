694208c469c2ead4366963e52cf58b5a2e55723a
rohit <rohitangsudas91@gmail.com>
2018-10-23 16:24:07 +0800

    done

--- bow.py
+++ bow.py
@@ -11,13 +11,27 @@ from nltk.tokenize import word_tokenize
 from nltk.stem.porter import PorterStemmer
 from gensim.models.doc2vec import TaggedDocument
 from nltk.stem import WordNetLemmatizer
+from collections import Counter 
+from nltk.corpus import wordnet # To get words in dictionary with their parts of speech
+
+
+def get_pos(word):
+        w_synsets = wordnet.synsets(word)
+        pos_counts = Counter()
+        pos_counts["n"] = len(  [ item for item in w_synsets if item.pos()=="n"]  )
+        pos_counts["v"] = len(  [ item for item in w_synsets if item.pos()=="v"]  )
+        pos_counts["a"] = len(  [ item for item in w_synsets if item.pos()=="a"]  )
+        pos_counts["r"] = len(  [ item for item in w_synsets if item.pos()=="r"]  )
+                                
+        most_common_pos_list = pos_counts.most_common(3)
+        return most_common_pos_list[0][0] # first indexer for getting the top POS from list, second indexer for getting POS from tuple( POS: count )
 
 def read_titles():
     """
         Function read the titles file and stores it in a list in the format (id, title)
         where id is the docid
     """
-    titles = []
+    titles = {}
     with open("titles", "r") as fp:
         for line in fp:
             try:
@@ -25,8 +39,10 @@ def read_titles():
             except:
                 splitted = line.lower().split("|")
                 id,sent = splitted[0], ' '.join(splitted[1:])
-                
-            titles.append((id, [sent.strip()]))
+
+            titles.update({
+                id: sent.strip()
+            })
     return titles
 
 def read_news_articles():
@@ -35,18 +51,27 @@ def read_news_articles():
         the files contain the articles in the following formats: id_1|article_1##id_2|article_2 ....
     """
     num_files = 4
+    titles =  read_titles()
     lines = []
     for num_file in range(num_files):
-        file_path = "test_data/Thread_" + str(num_file) + ".dat"
+        file_path = "test_data_v2/Thread_" + str(num_file) + ".dat"
         with open(file_path, "r") as fp:
             lines += fp.read().split("##")
     lines = map(lambda line: line.split("|"), lines)
-    lines =  filter(lambda tup: len(tup) == 2, lines)
-    #filter all those lines that have Empty as there article text
-    lines = filter(lambda tup: tup[1].strip() != 'Empty', lines)
-    #SPlit each article into lines.
-    lines = map(lambda tup: (tup[0], tup[1].lower().split("\n")), lines)
-    return lines
+    articles  = {}
+    empty = 0
+    for line in lines:
+        if len(line) >= 2:
+            id, others = line[0], ' '.join(line[1:]).strip()
+            if others == "Empty":
+                others = titles[id]
+                empty += 1
+            else:
+                others = titles[id] + " " + others
+            articles.update({
+                id: others.lower().split("\n")
+            })
+    return articles
 
 def preprocess(lines):
     """
@@ -98,7 +123,7 @@ def lemmatize(tokens_list):
     wordnet_lemmatizer = WordNetLemmatizer()
     lemmatized_list = []
     for token_list in tokens_list:
-        lemmatized_list.append([wordnet_lemmatizer.lemmatize(i) for i in token_list])
+        lemmatized_list.append([wordnet_lemmatizer.lemmatize(i, get_pos(i)) for i in token_list])
     return lemmatized_list
 
 def tag_docs(tokens_list):
@@ -132,9 +157,8 @@ def create_bow(stemmed_tokens):
         Creates the bow representation for tokens
     """
     texts = []
-    for id, lines in stemmed_tokens:
+    for lines in stemmed_tokens:
         texts += lines
-    print texts
     dictionary = corpora.Dictionary(texts)
     dictionary.save('dictionary.dict')
     
@@ -142,23 +166,26 @@ def create_bow(stemmed_tokens):
     #Get the doc 2 bag-of-words model
     bow_representation = []
     id_list = []
-    for id, lines in stemmed_tokens:
+    for lines in stemmed_tokens:
         para = []
         for line in lines:
             para += line
-        id_list.append(id)
         bow_representation.append(get_representation(dictionary.doc2bow(para), size_of_dictionary))
-    return np.array(id_list), np.array(bow_representation)
+    return np.array(bow_representation)
 
 def complete_preprocessing(lines):
     """
         Does a series of preprocessing steps.
     """
-    lines = map(lambda tup: (tup[0], preprocess(tup[1])) if tup[1] != 'Empty' else tup, lines)
-    lines = map(lambda tup: (tup[0], tokenize(tup[1])), lines)
-    lines = map(lambda tup: (tup[0], remove_stopwords(tup[1])), lines)
+    print("Precessing")
+    lines = map(lambda line: preprocess(line), lines)
+    print("Tokenizing")
+    lines = map(lambda line: tokenize(line), lines)
+    print("Removing Stop words")
+    lines = map(lambda line: remove_stopwords(line), lines)
     #lines = map(lambda tup: (tup[0], stem(tup[1])), stopword_removed_lines)
-    #lines = map(lambda tup: (tup[0], lemmatize(tup[1])), stopword_removed_lines)
+    print("Lemmatizing")
+    lines = map(lambda line: lemmatize(line), lines)
     return lines
 
 def augment_with_title(lines, all_lines):
@@ -177,25 +204,17 @@ def augment_with_title(lines, all_lines):
             out.append((id, text))
         curr_id += 1
     return out
-
+"""
 print("Reading articles")
-articles = read_news_articles()
-print("Reading titles")
-titles = read_titles()
-#print("Processing articles")
+articles_map = read_news_articles()
+ids, articles = [], []
+for id, article in articles_map.iteritems():
+    ids.append(id)
+    articles.append(article)
+
+print("Articles length: ", len(articles_map)) 
 prepreprocessed_articles = complete_preprocessing(articles)
-print("processing titles")
-preprocessed_titles = complete_preprocessing(titles)
-print("augmenting with titles")
-full_representation = augment_with_title(prepreprocessed_articles, preprocessed_titles)
-#print(full_representation)
-id, bow = create_bow(preprocessed_titles)
-print(id.shape, bow.shape)
-np.save("ids", id)
-np.save("bow_titles", bow)
-
-
-#print stopword_removed_titles
-#model = create_doc_two_vec(tagged_docs)
-#model.save("save/trained.model")
-#model.save_word2vec_format('save/trained.word2vec')
+bow = create_bow(prepreprocessed_articles)
+np.save("ids",  np.array(ids))
+np.save("bow_articles", bow)
+"""
--- word2vec.py
+++ word2vec.py
@@ -1,15 +1,58 @@
-from bow import read_news_articles, read_titles, complete_preprocessing, augment_with_title
+from bow import complete_preprocessing
 
 from gensim.models.doc2vec import Doc2Vec, TaggedDocument
 from nltk.tokenize import word_tokenize
 import numpy as np
 
+def read_titles():
+    """
+        Function read the titles file and stores it in a list in the format (id, title)
+        where id is the docid
+    """
+    titles = {}
+    with open("train_v_title", "r") as fp:
+        for line in fp:
+            id, title = line.lower().split("\t")
+            titles.update({
+                id: title.strip()
+            })
+    return titles
+
+def read_news_articles():
+    """
+        Function reads the news articles files : Thread_[0..3]
+        the files contain the articles in the following formats: id_1|article_1##id_2|article_2 ....
+    """
+    num_files = 6
+    titles =  read_titles()
+    lines = []
+    for num_file in range(num_files):
+        file_path = "test_data_v2/Thread_" + str(num_file) + ".dat"
+        with open(file_path, "r") as fp:
+            lines += fp.read().split("###")
+    lines = map(lambda line: line.split("||"), lines)
+    articles  = {}
+    empty = 0
+    for line in lines:
+        if len(line) >= 2:
+            id, others = line[0], ' '.join(line[1:]).strip()
+            if others == "Empty":
+                others = titles[id]
+                empty += 1
+            else:
+                others = titles[id] + " " + others
+            articles.update({
+                id: others.lower().split("\n")
+            })
+    return articles
+
 def tag_docs(tokens_list):
     tagged_docs = []
-    for index, tokens_sents in tokens_list:
+    for index, tokens_sents in enumerate(tokens_list):
         token_list = []
-        for tokens_sent in tokens_sents: 
-            token_list += tokens_sent
+        print tokens_sents
+        for token_sent in tokens_sents:
+            token_list += token_sent
         tagged_docs.append(TaggedDocument(words=token_list, tags=[str(index)]))
     return tagged_docs
 
@@ -18,13 +61,13 @@ def create_doc2vec(tag_docs):
     """
         Trains the doc2vec model
     """
-    max_epochs = 100                                
-    vec_size = 500                                            
+    max_epochs = 300                                
+    vec_size = 300                                            
     alpha = 0.025                          
     model = Doc2Vec(vector_size=vec_size,
                     alpha=alpha,                                                                               
                     min_alpha=0.025,
-                    min_count=0)
+                    min_count=1)
     model.build_vocab(tag_docs)
 
     for epoch in range(max_epochs):
@@ -41,21 +84,21 @@ def create_doc2vec(tag_docs):
 
 
 print("Reading articles")
-articles = read_news_articles()
-print("Reading titles")
-titles = read_titles()
+articles_map = read_news_articles()
+ids, articles = [], []
+for id, article in articles_map.iteritems():
+    ids.append(id)
+    articles.append(article)
 print("Processing articles")
 prepreprocessed_articles = complete_preprocessing(articles)
 print("processing titles")
-preprocessed_titles = complete_preprocessing(titles)
+#preprocessed_titles = complete_preprocessing(titles)
 print("augmenting with titles")
-full_representation = augment_with_title(prepreprocessed_articles, preprocessed_titles)
-tag_docs = tag_docs(full_representation)
+#full_representation = augment_with_title(prepreprocessed_articles, preprocessed_titles)
+tag_docs = tag_docs(prepreprocessed_articles)
 print tag_docs[0]
 model = create_doc2vec(tag_docs)
-print(model.docvecs.vectors_docs)
-print(model.docvecs[2000])
 np.save("word2vec", model.docvecs.vectors_docs)
+np.save("ids", np.array(ids))
 print len(tag_docs)
-
 
