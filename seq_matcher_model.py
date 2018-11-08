1735534ce64352a24fa67d63f09406f2393d5fa5
unknown <workshop@soccf-mrl4-010.comp.nus.edu.sg>
2018-11-08 18:51:53 +0800

    two submissions done

--- seq_matcher_model.py
+++ seq_matcher_model.py
@@ -1,3 +1,4 @@
+import csv
 import re
 import difflib
 from collections import Counter
@@ -8,18 +9,19 @@ from nltk.corpus import wordnet
 from nltk import WordNetLemmatizer
 from sklearn.metrics import accuracy_score
 
-TOP_N = 3
+TOP_N = 5
+SIMILARITY_THRESHOLD = 50.0
 
 
 def get_pos(word):
-    w_synsets = wordnet.synsets (word)
-    pos_counts = Counter ()
-    pos_counts["n"] = len ([item for item in w_synsets if item.pos () == "n"])
-    pos_counts["v"] = len ([item for item in w_synsets if item.pos () == "v"])
-    pos_counts["a"] = len ([item for item in w_synsets if item.pos () == "a"])
-    pos_counts["r"] = len ([item for item in w_synsets if item.pos () == "r"])
+    w_synsets = wordnet.synsets(word)
+    pos_counts = Counter()
+    pos_counts["n"] = len([item for item in w_synsets if item.pos() == "n"])
+    pos_counts["v"] = len([item for item in w_synsets if item.pos() == "v"])
+    pos_counts["a"] = len([item for item in w_synsets if item.pos() == "a"])
+    pos_counts["r"] = len([item for item in w_synsets if item.pos() == "r"])
 
-    most_common_pos_list = pos_counts.most_common (3)
+    most_common_pos_list = pos_counts.most_common(3)
     return most_common_pos_list[0][
         0]  # first indexer for getting the top POS from list, second indexer for getting POS from tuple( POS: count )
 
@@ -28,18 +30,22 @@ def lemmatize(tokens_list):
     """
         Uses WordNet lemmatizer to lemmatize
     """
-    wordnet_lemmatizer = WordNetLemmatizer ()
+    wordnet_lemmatizer = WordNetLemmatizer()
     lemmatized_list = []
     for i in tokens_list:
-        lemmatized_list.append (wordnet_lemmatizer.lemmatize (i, get_pos (i)))
+        lemmatized_list.append(wordnet_lemmatizer.lemmatize(i, get_pos(i)))
     return lemmatized_list
 
 
 def preprocess(text):
     # Remove non-alphanumeric characters
-    processed_line = re.sub (r'\W+', ' ', text).strip ()
+    processed_line = re.sub(r'\W+', ' ', text).strip()
     # Remove all digits
-    processed_line = re.sub (r'\w*\d\w*', '', processed_line).strip ()
+    processed_line = re.sub(r'\w*\d\w*', '', processed_line).strip()
+    # Remove non-ascii characters
+    processed_line = processed_line.encode("ascii", errors="ignore").decode()
+    # Lemmatize
+    processed_line = lemmatize(processed_line.split(" "))
     return processed_line
 
 
@@ -49,13 +55,13 @@ def read_train_titles():
         where id is the docid
     """
     titles = {}
-    with open ("newdata/train_v_title", "r") as fp:
+    with open("newdata/train_v_title", "r") as fp:
         for line in fp:
-            id, sent = line.lower ().split ("\t")
-            titles.update ({
-                id: sent.strip ()
+            id, sent = line.lower().split("\t")
+            titles.update({
+                id: sent.strip()
             })
-    print ("Length of titles: ", len (titles))
+    print("Length of titles: ", len(titles))
     return titles
 
 
@@ -65,28 +71,84 @@ def read_train_keywords():
         the files contain the articles in the following formats: id_1|article_1##id_2|article_2 ....
     """
     num_files = 6
-    titles = read_train_titles ()
+    titles = read_train_titles()
     lines = []
-    for num_file in range (num_files):
-        file_path = "newdata/Thread_keywords_" + str (num_file) + ".dat"
-        with open (file_path, "r") as fp:
-            lines += fp.read ().split ("###")
-    lines = map (lambda line: line.split ("||"), lines)
+    for num_file in range(num_files):
+        file_path = "newdata/Thread_keywords_" + str(num_file) + ".dat"
+        with open(file_path, "r") as fp:
+            lines += fp.read().split("###")
+    lines = map(lambda line: line.split("||"), lines)
     keywords = {}
     empty = 0
-    ids_set = set ()
+    ids_set = set()
     for line in lines:
-        if len (line) >= 2:
-            id, others = line[0], ' '.join (line[1:]).strip ()
+        if len(line) >= 2:
+            id, others = line[0], ' '.join(line[1:]).strip()
             if others == "Empty":
                 others = titles[id]
                 empty += 1
             else:
-                others = titles[id] + " " + re.sub (",", " ", others)
-            keywords.update ({
-                id: preprocess (others.lower ())
+                others = titles[id] + " " + re.sub(",", " ", others)
+            keywords.update({
+                id: preprocess(others.lower())
             })
-            ids_set.add (id)
+            ids_set.add(id)
+    # print ("Missing ids: ", set (titles.keys ()).difference (ids_set))
+
+    # filter all those lines that have Empty as there article text
+    return keywords
+
+
+def read_test_titles():
+    """
+        Function read the titles file and stores it in a list in the format (id, title)
+        where id is the docid
+    """
+    titles = {}
+    with open("test_v2.csv", "r") as fp:
+        first_line = True
+        for line in fp:
+            if first_line:
+                first_line = False
+                continue
+
+            csv_row = line.lower().split(",")
+            print(csv_row)
+            titles.update({
+                csv_row[0]: csv_row[1].strip()  # csv_row[0] = ID, csv_row[1] = Title
+            })
+    print("Length of test titles: ", len(titles))
+    return titles
+
+
+def read_test_keywords():
+    """
+        Function reads the news articles' keywords files : Thread_[0..5]
+        the files contain the articles in the following formats: id_1|article_1##id_2|article_2 ....
+    """
+    num_files = 6
+    titles = read_test_titles()
+    lines = []
+    for num_file in range(num_files):
+        file_path = "newtestdata/Thread_keywords_" + str(num_file) + ".dat"
+        with open(file_path, "r") as fp:
+            lines += fp.read().split("###")
+    lines = map(lambda line: line.split("||"), lines)
+    keywords = {}
+    empty = 0
+    ids_set = set()
+    for line in lines:
+        if len(line) >= 2:
+            id, others = line[0], ' '.join(line[1:]).strip()
+            if others == "Empty":
+                others = titles[id]
+                empty += 1
+            else:
+                others = titles[id] + " " + re.sub(",", " ", others)
+            keywords.update({
+                id: preprocess(others.lower())
+            })
+            ids_set.add(id)
     # print ("Missing ids: ", set (titles.keys ()).difference (ids_set))
 
     # filter all those lines that have Empty as there article text
@@ -99,46 +161,80 @@ def predict(X_train, y_train, X_test):
     for test_sample in X_test:
         # print ('===================================================================')
         # print ('>> ' + test_sample)
-        similar_list_above_50 = []
-        similar_list_below_50 = []
-        for train_label, train_title in zip (y_train, X_train):
+        similar_list_above_th = []
+        similar_list_below_th = []
+        similarity_list = []
+        for train_label, train_title in zip(y_train, X_train):
             # print(train_title)
             # print(test_sample)
-            seq = difflib.SequenceMatcher (None, train_title.split (' '), test_sample.split (' '))
-            similarity = seq.ratio () * 100
-            if similarity > 50.0:
-                similar_list_above_50.append ([train_label, similarity])
+            seq = difflib.SequenceMatcher(None, train_title, test_sample)
+            similarity = seq.ratio() * 100
+            # similarity_list.append([train_label, similarity])
+            if similarity > SIMILARITY_THRESHOLD:
+                similar_list_above_th.append([train_label, similarity])
                 # print (str (train_label) + " {:0.2f} ".format (similarity) + ' ' + train_title)
             else:
-                similar_list_below_50.append ([train_label, similarity])
-        if len (similar_list_above_50) > 0:
-            top_similarities = np.array (sorted (similar_list_above_50, key=lambda t: t[1], reverse=True)[:TOP_N])
+                similar_list_below_th.append([train_label, similarity])
+        if len(similar_list_above_th) > 0:
+            top_similarities = np.array(sorted(similar_list_above_th, key=lambda t: t[1], reverse=True)[:TOP_N])
         else:
-            top_similarities = np.array (sorted (similar_list_below_50, key=lambda t: t[1], reverse=True)[:TOP_N])
-        top_labels = np.reshape (top_similarities[:, 0], -1)
-        counter = Counter (top_labels.tolist ())
-        predicted_label, majority_count = counter.most_common ()[0]
-        y_pred.append (predicted_label)
+            top_similarities = np.array(sorted(similar_list_below_th, key=lambda t: t[1], reverse=True)[:1])
+        # top_similarities = np.array(sorted(similarity_list, key=lambda t: t[1], reverse=True)[:TOP_N])
+        top_labels = np.reshape(top_similarities[:, 0], -1)
+        counter = Counter(top_labels.tolist())
+        predicted_label, majority_count = counter.most_common()[0]
+        y_pred.append(predicted_label)
         progress += 1
-        print (str (progress) + '/' + str (len (X_test)))
+        print(str(progress) + '/' + str(len(X_test)))
     return y_pred
 
 
+# Reads old labels
+def read_labels():
+    category = {}
+    with open("newdata/train_v_category", "r") as fp:
+        for line in fp:
+            id, sent = line.lower().split("\t")
+            category.update({
+                id: int(sent.strip())
+            })
+    return category
+
+
 # Preparing Data
-ids, keywords = zip (*read_train_keywords ().items ())
-# print(keywords[0])
-labels = np.reshape (np.load ('train_corrected.npy')[:, [1]], -1).tolist ()
+ids, keywords = zip(*read_train_keywords().items())
+print(keywords[0])
+# Corrected New labels
+# labels = np.reshape(np.load('train_corrected.npy')[:, [1]], -1).tolist()
 
-# # Preparing Test Data
+# Old labels
+cat_dict = read_labels()
+labels = []
+for id in ids:
+    labels.append(cat_dict[id])
+
+# Preparing Test Data
+test_ids, test_keywords = zip(*read_test_keywords().items())
+# print(test_keywords[0])
 # test_df = pd.read_csv ("test_v2.csv", skiprows=[0], header=None)
-# test_titles = preprocess (test_df[test_df.columns[1]])
+# test_titles = preprocess(test_df[test_df.columns[1]])
 
-
-X_train, X_test, y_train, y_test = model_selection.train_test_split (keywords, labels, test_size=0.05)
+# Test Train Split
+# X_train, X_test, y_train, y_test = model_selection.train_test_split(keywords, labels, test_size=0.20)
 # print(len(X_test))
 # print(len(y_test))
 
-y_pred = predict (X_train, y_train, X_test)
+# Prediction
+y_pred = predict(keywords, labels, test_keywords)
+# y_pred = predict(X_train, y_train, X_test)
 # print(len(y_pred))
-acc = accuracy_score (y_test, y_pred)
-print ('Accuracy: ' + str (acc))
+# acc = accuracy_score(y_test, y_pred)
+# print('Accuracy: ' + str(acc))
+
+# Save submission csv
+np.save('seq_matcher_submission_n_5_old.npy', np.array(y_pred))
+# with open('seq_matcher_submission.csv', 'w') as f:
+#     writer = csv.writer(f)
+#     writer.writerow(['article_id', 'category'])
+#     for row in zip(ids, y_pred):
+#         writer.writerow(row)
